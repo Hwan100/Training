2c934ef4425490ff7f34e56a0df88114b34cd69a
Hwan <32670776+Hwan100@users.noreply.github.com>
2026-07-24 10:27:35 +0900

    [level 1] Title: 달리기 경주, Time: 205.96 ms, Memory: 368 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < players.length; i++){
+            map.put(players[i], i);
+        }
+        
+        for(String call : callings){
+            int idx = map.get(call);
+            
+            String temp = players[idx - 1];
+            players[idx - 1] = call;
+            players[idx] = temp;
+            
+            map.put(call, idx-1);
+            map.put(temp, idx);
+        }
+        
+        return players;
+    }
+}
